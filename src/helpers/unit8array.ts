75f482440adfe8a844626d6adf645ef137839e81
Jinnan Zhang <vannizhang@gmail.com>
2023-12-13 10:41:56 -0800

    areUint8ArraysEqual should do comparison from end of array

--- src/helpers/unit8array.ts
+++ src/helpers/unit8array.ts
@@ -3,7 +3,9 @@ export const areUint8ArraysEqual = (arr1: Uint8Array, arr2: Uint8Array) => {
         return false;
     }
 
-    for (let i = 0; i < arr1.length; i++) {
+    // start comparison from the end of the array as the first several hundreds of items in those array
+    // will likely be identitical for most of tile images
+    for (let i = arr1.length - 1; i >= 0; i--) {
         if (arr1[i] !== arr2[i]) {
             return false;
         }
